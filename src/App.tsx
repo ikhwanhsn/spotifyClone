e70a7077b42e2bac48e9c9fdcc03ab648ccf7246
ikhwanhsn <ikhwanulhusna111@gmail.com>
2023-11-04 11:29:27 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+      <div className="mt-32 text-center">
+        <h1 className="text-3xl font-bold">
+          Template Vite (React + Typescript + Tailwind)
+        </h1>
+        <button className='px-5 py-2 mt-5 text-white bg-blue-500 rounded-full hover:bg-blue-600'>Check Version</button>
+      </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
